69d9037ac1fbdec61573b64173cb371ebe1f0e15
emre kayacan <emre.kayacan@msn.com>
2020-10-12 02:14:34 +0300

    user controller created

---
+++ server/src/user/controllers/user.controller.ts
@@ -0,0 +1,39 @@
+import { Observable } from 'rxjs';
+import { IUser } from './../models/user.interface';
+import { UserService } from './../services/user.service';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() user: IUser): Observable<IUser> {
+    return this.userService.create(user);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Observable<IUser> {
+    return this.userService.findOne(id);
+  }
+  @Get()
+  findAll(): Observable<IUser[]> {
+    return this.userService.findAll();
+  }
+  @Delete(':id')
+  deleteOne(@Param('id') id: number): Observable<any> {
+    return this.userService.deleteOne(id);
+  }
+  @Put(':id')
+  updateOne(@Param('id') id: number, @Body() user: IUser): Observable<any> {
+    return this.userService.updateOne(id, user);
+  }
+}
