ab028d617df8aaa5224993e636888301969cafe8
emre kayacan <emre.kayacan@msn.com>
2020-10-12 20:51:41 +0300

    create new user service func ok

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -1,18 +1,43 @@
-import { from, Observable } from 'rxjs';
+import { catchError, switchMap, map } from 'rxjs/operators';
+import { Observable, throwError, from, of } from 'rxjs';
 import { IUser } from './../models/user.interface';
 import { UserEntity } from './../models/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AuthService } from 'src/auth/services/auth.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private readonly authService: AuthService,
   ) {}
-  create(user: IUser): Observable<IUser> {
-    return from(this.userRepository.save(user));
+  create(user: IUser): Observable<IUser | { error: string }> {
+    const { username, password } = user;
+    if (username && username.length > 3 && password && password.length > 3) {
+      return this.authService.hashPassword(password).pipe(
+        switchMap((hashedPass: string) => {
+          const newUser = new UserEntity();
+          newUser.username = username;
+          newUser.password = hashedPass;
+          return from(this.userRepository.save(newUser)).pipe(
+            map((user: IUser) => {
+              console.log(user);
+              const { password, ...result } = user;
+              return result;
+            }),
+            catchError(err => of({ error: err.message })),
+          );
+        }),
+        catchError(err => throwError(err.message)),
+      );
+    } else {
+      return throwError(
+        'Username and password lengths must be between 3 and 10 characters!',
+      );
+    }
   }
   findOne(id: number): Observable<IUser> {
     return from(this.userRepository.findOne(id));
