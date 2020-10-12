10c7097b624fcf77f130a61ad0fe6973ca3c48b0
emre kayacan <emre.kayacan@msn.com>
2020-10-13 00:24:31 +0300

    user entity edited

--- server/src/user/models/user.entity.ts
+++ server/src/user/models/user.entity.ts
@@ -1,14 +1,24 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({ unique: true, nullable: false })
+  username: string;
 
-    @Column({unique:true,nullable:false})
-    username: string;
+  @Column()
+  password: string;
 
-    @Column()
-    password: string;
+  @CreateDateColumn({ nullable: false })
+  createdAt: Date;
+
+  @CreateDateColumn({ nullable: false })
+  updatedAt: Date;
 }
