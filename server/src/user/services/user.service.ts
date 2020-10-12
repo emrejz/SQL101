48fdd61dd3bc0d3304a8230d59c4c801fbfba0c8
emre kayacan <emre.kayacan@msn.com>
2020-10-13 00:25:40 +0300

    updateOne and deleteOne user service funcs ok

--- server/src/user/services/user.service.ts
+++ server/src/user/services/user.service.ts
@@ -15,54 +15,91 @@ export class UserService {
     private readonly authService: AuthService,
   ) {}
   create(user: IUser): Observable<IUser | { error: string }> {
-    const { username, password } = user;
-    if (username && username.length > 3 && password && password.length > 3) {
-      return this.authService.hashPassword(password).pipe(
-        switchMap((hashedPass: string) => {
-          const newUser = new UserEntity();
-          newUser.username = username;
-          newUser.password = hashedPass;
-          return from(this.userRepository.save(newUser)).pipe(
-            map((user: IUser) => {
-              const { password, ...result } = user;
-              return result;
-            }),
-            catchError(err => of({ error: err.message })),
-          );
-        }),
-        catchError(err => of({ error: err.message })),
-      );
-    } else {
-      return of({
-        error:
-          'Username and password lengths must be between 3 and 10 characters!',
-      });
+    let { username, password } = user;
+    if (username && password) {
+      username = username.trim();
+      password = password.trim();
+      if (username.length > 3 && password.length > 3) {
+        return this.authService.hashPassword(password).pipe(
+          switchMap((hashedPass: string) => {
+            const newUser = new UserEntity();
+            newUser.username = username;
+            newUser.password = hashedPass;
+            return from(this.userRepository.save(newUser)).pipe(
+              map((user: IUser) => this.safeUserResponse(user)),
+              catchError(err => of({ error: err.message })),
+            );
+          }),
+          catchError(err => of({ error: err.message })),
+        );
+      }
     }
+    return of({
+      error:
+        'Username and password lengths must be between 3 and 10 characters!',
+    });
   }
   findOne(id: number): Observable<IUser | { error: string }> {
     return from(this.userRepository.findOne(id)).pipe(
-      map(user => {
-        const { password, ...result } = user;
-        return result;
-      }),
+      map((user: IUser) => this.safeUserResponse(user)),
       catchError(err => of({ error: err.message })),
     );
   }
   findAll(): Observable<IUser[] | { error: string }> {
     return from(this.userRepository.find()).pipe(
       map((users: IUser[]) => {
-        return users.map(item => {
-          const { password, ...result } = item;
-          return result;
-        });
+        return users.map((user: IUser) => this.safeUserResponse(user));
       }),
       catchError(err => of({ error: err.message })),
     );
   }
-  deleteOne(id: number): Observable<any> {
-    return from(this.userRepository.delete(id));
+  deleteOne(id: number): Observable<IUser | { error: string }> {
+    return from(this.findOne(id)).pipe(
+      switchMap((user: IUser) => {
+        return from(this.userRepository.delete(id)).pipe(
+          map(res => user),
+          catchError(err => of({ error: err.message })),
+        );
+      }),
+    );
   }
-  updateOne(id: number, user: IUser): Observable<any> {
-    return from(this.userRepository.update(id, user));
+  updateOne(id: number, user: IUser): Observable<IUser | { error: string }> {
+    let { username } = user;
+    if (username) {
+      username = username.trim();
+      if (username.length > 3) {
+        return from(this.userRepository.findOne({ where: { username } })).pipe(
+          switchMap(result => {
+            if (!result) {
+              return from(
+                this.userRepository.update(id, {
+                  username,
+                  updatedAt: new Date(),
+                }),
+              ).pipe(
+                map(res => user),
+                catchError(err => of({ error: err.message })),
+              );
+            } else {
+              return of({
+                error: 'Username already exists. Please try another one!',
+              });
+            }
+          }),
+          catchError(err => of({ error: err.message })),
+        );
+      }
+    }
+    return of({
+      error: 'Username length must be between 3 and 10 characters!',
+    });
+  }
+  safeUserResponse(user: IUser) {
+    if (user) {
+      const { password, ...result } = user;
+      return result;
+    } else {
+      throw new Error('User does not exist!');
+    }
   }
 }
