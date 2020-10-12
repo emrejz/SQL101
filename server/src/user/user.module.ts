d548f84bcba7fc86e0f9beeed714e1eee4f3c4d6
emre kayacan <emre.kayacan@msn.com>
2020-10-12 20:50:33 +0300

    authModule imported on userModule

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './controllers/user.controller';
 import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
