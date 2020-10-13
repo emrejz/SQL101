d450fd82a93f99aae5e46c3270cbf1e096f7515a
emre kayacan <emre.kayacan@msn.com>
2020-10-13 14:14:21 +0300

    user controller edited and working good

--- server/src/user/controllers/user.controller.ts
+++ server/src/user/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { IUser } from './../models/user.interface';
 import { UserService } from './../services/user.service';
 import {
@@ -17,23 +18,49 @@ export class UserController {
 
   @Post()
   create(@Body() user: IUser): Observable<IUser | { error: string }> {
-    return this.userService.create(user);
+    return this.userService.create(user).pipe(
+      map(res => res),
+      catchError(err => of({ error: err.message })),
+    );
   }
-
+  @Post('login')
+  login(
+    @Body() user: IUser,
+  ): Observable<{ token: string } | { error: string }> {
+    return this.userService.login(user).pipe(
+      map(token => ({ token })),
+      catchError(err => of({ error: err.message })),
+    );
+  }
   @Get(':id')
   findOne(@Param('id') id: number): Observable<IUser | { error: string }> {
-    return this.userService.findOne(id);
+    return this.userService.findOne(id).pipe(
+      map(res => res),
+      catchError(err => of({ error: err.message })),
+    );
   }
   @Get()
   findAll(): Observable<IUser[] | { error: string }> {
-    return this.userService.findAll();
+    return this.userService.findAll().pipe(
+      map(res => res),
+      catchError(err => of({ error: err.message })),
+    );
   }
   @Delete(':id')
-  deleteOne(@Param('id') id: number): Observable<any> {
-    return this.userService.deleteOne(id);
+  deleteOne(@Param('id') id: number): Observable<IUser | { error: string }> {
+    return this.userService.deleteOne(id).pipe(
+      map(res => res),
+      catchError(err => of({ error: err.message })),
+    );
   }
   @Put(':id')
-  updateOne(@Param('id') id: number, @Body() user: IUser): Observable<any> {
-    return this.userService.updateOne(id, user);
+  updateOne(
+    @Param('id') id: number,
+    @Body() user: IUser,
+  ): Observable<IUser | { error: string }> {
+    return this.userService.updateOne(id, user).pipe(
+      map(res => res),
+      catchError(err => of({ error: err.message })),
+    );
   }
 }
